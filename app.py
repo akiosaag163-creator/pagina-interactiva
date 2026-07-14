ea6043d2ba5e2c30b2aef64b07d7466ff9c663bb
akiosaag163-creator <akiosaag163@gmail.com>
2026-07-13 21:00:57 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import google.generativeai as genai
+
+# Esto es para probar si el archivo es válido
+st.title("Prueba de Chatbot")
+st.write("Si ves esto, el archivo es válido.")
